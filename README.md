d102f9af6dd92f7a5579ed46d1c78cb7c7294de3
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-29 01:17:11 -0400

    added ejs

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ npm i express bcryptjs passport passport-local ejs express-ejs-layouts mongoose 
 
 1. create express app
 2. setup routes
+
+
+Notes:
+A template engine allows you use static template files in your application. At runtime, the template engine replaces variables
+in a template file with actual values, and transforms the template into an HTML file sent to the client. This makes it easier 
+to design an HTML page.
+    - In this project, we use ejs
+    ```
+    const expressLayouts = require('express-ejs-layouts')
+    app.use(expressLayouts)
+    app.use('view engine', 'ejs')
+    ```
+
--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express')
 //route imports
 const usersRoute = require('./routes/users')
 const indexRoute = require('./routes/index') 
-
+//ejs
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
-//process.env.PORT is for when we deploy, otherwise we just use port 5000
-const PORT = process.env.PORT || 5000
+//EJS
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
 
 //routes
 app.use('/', indexRoute)
 app.use('/users', usersRoute)
+
+//process.env.PORT is for when we deploy, otherwise we just use port 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>console.log(`listening at ${PORT}`))
